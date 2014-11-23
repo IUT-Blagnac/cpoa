abb9db2a779d5be3a72045090e0500a5eff41c12
Jean-Michel Bruel <jbruel#travis@gmail.com>
2014-11-23 15:51:43 +0100

    pushing TP1 modifs

--- src/java/SuperCanardBof/src/Colvert.java
+++ src/java/SuperCanardBof/src/Colvert.java
@@ -1,9 +1,10 @@
+/**
+ * @author bruel
+ *
+ */
 public class Colvert extends Canard {
 
-	public Colvert() {
-		comportementCancan = new Cancan();
-		comportementVol = new VolerAvecDesAiles();
-	}
+	@Override
 	public void afficher() {
 		System.out.println("Je suis un Colvert");
 	}
--- src/java/SuperCanardBof/src/SuperCanard.java
+++ src/java/SuperCanardBof/src/SuperCanard.java
@@ -1,11 +1,17 @@
+/**
+ * @author bruel
+ * @navassoc - - 1 Canard
+ * @note Main de test simple
+ *
+ */
 public class SuperCanard {
 
 	public static void main(String[] args) {
 		Canard c1 = new Colvert();
-		c1.effectuerCancan();
+		c1.cancaner();
 		c1.nager();
 		c1.afficher();
-		c1.effectuerVol();
+		c1.voler();
 	}
 
 }
