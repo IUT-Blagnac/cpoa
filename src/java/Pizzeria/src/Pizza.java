c402b86194a9b945c14060d8f82e372943ee7d56
Jean-Michel Bruel <jbruel#travis@gmail.com>
2014-11-27 09:25:03 +0100

    TD3 et TP2

--- src/java/Pizzeria/src/Pizza.java
+++ src/java/Pizzeria/src/Pizza.java
@@ -6,6 +6,31 @@
  * @author bruel
  *
  */
-public class Pizza {
-
+public abstract class Pizza { 
+	String nom;
+	String pate;
+	String sauce;
+	ArrayList garnitures = new ArrayList();
+	
+	void preparer() { 
+		System.out.println("Préparation de " + nom); 
+		System.out.println("Étalage de la pâte..."); 
+		System.out.println("Ajout de la sauce..."); 
+		System.out.println("Ajout des garnitures: "); 
+			for (int i = 0; i < garnitures.size(); i++) {
+		System.out.println(" " + garnitures.get(i)); }
+	}
+	void cuire() {
+		System.out.println("Cuisson 25 minutes à 180°");
+	}
+	void couper() {
+		System.out.println("Découpage en parts triangulaires");
+	}
+	void emballer() {
+		System.out.println("Emballage dans une boîte officielle");
+		￼￼￼￼￼}
+	public String getNom() { 
+		return nom;
+	} 
 }
+
--- src/java/Pizzeria/src/PizzeriaTest.java
+++ src/java/Pizzeria/src/PizzeriaTest.java
@@ -2,6 +2,15 @@
  * @author bruel
  *
  */
-public class PizzeriaTest {
-
+public class PizzaTestDrive {
+	public static void main(String[] args) {
+		Pizzeria boutiqueBrest = new PizzeriaBrest();
+		Pizzeria boutiqueStrasbourg = new PizzeriaStrasbourg();
+		
+		Pizza pizza = boutiqueBrest.commanderPizza("fromage”); 
+		System.out.println("Luc a commandé une " + pizza.getNom() + "\n”);
+		
+		pizza = boutiqueStrasbourg.commanderPizza("fromage”);
+		System.out.println("Michel a commandé une " + pizza.getNom() + "\n”); 
+	}
 }
